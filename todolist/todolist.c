fb08ef8ed582fb76e3a72edd91b8e89b1a79c8d6
Eustone Malaya <emalaya@gmail.com>
2025-01-11 19:59:03 +0200

    Added structure to project

--- todolist/todolist.c
+++ todolist/todolist.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 
-#define MAX_TASKS 10;
+#define MAX_TASKS 10
+#define MAX_DESC_LENGTH 100
+
+typedef struct todolist
+{   
+    int id;
+    char description[MAX_DESC_LENGTH];
+    bool is_completed;
+
+}TODOLIST;
+
 
 int main()
 {
